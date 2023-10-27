8400e90aedbc56d391a4c16fbd1c95279ed1eb14
Игорь Алиев <aliev.igor1@mail.ru>
2023-10-28 00:13:12 +0300

    Centralities were calculated

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import networkx as nx
-import scipy as sp
 import matplotlib.pyplot as plt
+import csv
 
-f = open("C:\\Users\\aliev\source\\repos\FriendsFriend\FriendsFriend\\bin\Debug\\net7.0\data\\Vertex.txt", "r")
+f1 = csv.reader(open("C:\\Users\\aliev\\Downloads\\Chinim_bitoe.csv"))
 G = nx.Graph()
+i=0
 
-for c in f.readlines():
-    G.add_node(c)
+for row in f1:
+    if (i < 3240):
+        i+=1
+        G.add_nodes_from(row)
+        G.add_edge(row[0], row[1])
 
-print(G)
-nx.draw(G)
-plt.savefig("filename.png")
+#plt.figure(3, figsize=(10, 10))
+#nx.draw(G, node_size=1, pos=nx.shell_layout(G))
+#plt.show()
+
+bet_centr = nx.betweenness_centrality(G)
+clo_centr = nx.closeness_centrality(G)
+#eig_centr = nx.eigenvector_centrality(G)
+
+print(bet_centr)
+print(clo_centr)
+#print(eig_centr)
+
+results = [(k,bet_centr[k],clo_centr[k]) #,eig_centr[k]
+
+for k in G]
+f = open('Centrality', 'w')
+for item in results:
+    f.write(','.join(map(str,item)))
+    f.write('\n')
+f.close()
